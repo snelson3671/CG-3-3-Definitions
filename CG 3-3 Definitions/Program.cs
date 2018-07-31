1236cf881698554236e1f18373c4e6b7c2e85201
Sandra <snelson3671@yahoo.com>
2018-07-30 22:16:06 -0500

    Add project files.

---
+++ CG 3-3 Definitions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CG_3_3_Definitions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
